3f91ec37bdfe8ecca1dc0d9f24e8328c768def8a
victorcodebase <victorkithinji@outlook.com>
2026-04-13 13:11:21 +0300

    terminal imag update

--- src/pages/docs/hex-stream-terminal.js
+++ src/pages/docs/hex-stream-terminal.js
@@ -159,7 +159,7 @@ export default function HexStreamTerminalDocs() {
 						</p>
 						<div className="relative w-full my-6 h-84 bg-gray-200 rounded-lg border-2 border-dashed border-gray-300 flex items-center justify-center overflow-hidden text-gray-500">
 							<Image
-								src="/custom-terminal/terminal_img_banner_01.jpeg"
+								src="/custom-terminal/terminal_img_banner.jpeg"
 								alt="Hex Stream Terminal banner"
 								fill
 								className="object-cover rounded-lg p-2"
